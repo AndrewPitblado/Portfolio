3e97dc716d87601e3a1d53c6d8f040d42f30abb3
Andrew Scott Pitblado <apitblado@gmail.com>
2026-03-11 12:36:02 -0400

    Removed @astojs/tailwindcss due to V6.0 update, migrated over to postcss + autoprexifer, added global tailwind css file, updated astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react()],
   vite: {
     resolve: {
       alias: {
-        '@': '/src',
-        '@components': '/src/components'
-      }
-    }
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
   },
-  output: 'static',
+  output: "static",
   build: {
-    inlineStylesheets: 'auto'
+    inlineStylesheets: "auto",
   },
   server: {
     host: true,
     port: 4321,
-  }
+  },
 });
